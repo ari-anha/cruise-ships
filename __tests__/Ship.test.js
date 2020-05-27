c8a3e34f0016ca5dbeb13defde243ded93b6fc61
Arianha Bayley <arianhamb@gmail.com>
2020-05-27 16:49:57 +0100

    Wrote constructor function and test

---
+++ __tests__/Ship.test.js
@@ -0,0 +1,8 @@
+/* globals descripe it expect*/
+const Ship = require('../src/Ship.js')
+
+describe("Ship", () => {
+    it("can be instantiated", () => {
+        expect(new Ship()).toBeInstanceOf(Object);
+    });
+});
