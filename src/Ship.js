f258057c609da9e46af0a425260adf43c3675e8c
Arianha Bayley <arianhamb@gmail.com>
2020-05-28 20:54:31 +0100

    Added a setSail() function

--- src/Ship.js
+++ src/Ship.js
@@ -1,6 +1,11 @@
 function Ship(startingPort) {
     this.startingPort = startingPort;
     this.passengerNos = 0;
+};
+
+Ship.prototype.setSail = function() {
+//ship.startingport needs to be falsy
+    this.startingPort = "";
 }
 
 module.exports = Ship;
